e94ecd1e62fed51ad93c72165e120ba662f09d75
eichlee j <jeichlee@jeichleeui-macbook-pro.local>
2017-03-07 21:05:44 +0900

    Initial Commit

---
+++ tpayOprTestApk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tpayOprTestApk
+//
+//  Created by jeichlee on 2017. 3. 7..
+//  Copyright © 2017년 jeichlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tpayOprTestApk/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  tpayOprTestApk
+//
+//  Created by jeichlee on 2017. 3. 7..
+//  Copyright © 2017년 jeichlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
