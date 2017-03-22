3464ca8ba61f17836e933e18e8f78fcd8af5b8cc
Kyungsoo.Cho <jojo5166@172.20.nate.com>
2017-03-22 16:12:58 +0900

    aa

---
+++ tpayOprTestApk/Library/NSString+MagicSEUtil.h
@@ -0,0 +1,18 @@
+//
+//  NSString+MagicSEUtil.h
+//  tpayOprTestApk
+//
+//  Created by Elite on 2017. 3. 9..
+//  Copyright © 2017년 jeichlee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (MagicSEUtil)
+
+-(NSString *) MagicSE_Enc:(NSString *)str;
+
+void CASE_1( );
+
+
+@end
